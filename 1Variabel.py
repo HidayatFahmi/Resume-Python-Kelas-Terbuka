bf028a178bc31ebadd35448c40dc8c75c3846481
fahmi hidayat <hidayat.fh22@gmail.com>
2025-10-02 17:49:52 +0700

    update 1 type data materi

--- 1Variabel.py
+++ 1Variabel.py
@@ -1,5 +1,11 @@
 # assignment nilai pada varibel
-a = 55;
-b = 77;
+a = 55
+b = 77
 z= a+b; 
 print("nilai z adalah", z);
+
+# Type Data
+data_integer = 22
+data_float = 9.8
+print("data integer :", data_integer,"bertipe : ", type(data_integer))
+print("data float", data_float," bertipe : ", type(data_float))
