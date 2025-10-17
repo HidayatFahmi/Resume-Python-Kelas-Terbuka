64afa8166edc235edf9a2b852ee109a84ae7576e
fahmi hidayat <hidayat.fh22@gmail.com>
2025-10-18 06:24:54 +0700

    latihan perasi

--- 4Aritmatika.py
+++ 4Aritmatika.py
@@ -3,11 +3,17 @@ bilangan2 = int(input("masukkan bilangan 2 : "))
 hasil = bilangan1 + bilangan2
 print(f"{bilangan1} + {bilangan2} = {hasil}")
 
+# Exponential (perpangkatan)
+print(f"4 pangakat 3 = {4**3}")
+
 # Modulus
-print(f"modulus dari 4 % 3 = {4%3}")
+print(f"modulus dari 44 % 31 = {44%31}")
+
+# Division
+print(f"44 / 31 = {44/31}")
 
 # Floor Division
 angka1 = 17
 angka2 = 6
 hasil2 = angka1 // angka2
-print(f"floor division dari {angka1} / {angka2} = {hasil2}")
+print(f"floor division dari {angka1} // {angka2} = {hasil2}")
---
+++ 5LatihanAritmatika.py
@@ -0,0 +1,4 @@
+#  Konversi Temperature
+# Celcius to other units
+print("Program Konverdsi Temperature")
+celcius_value = float(input("Masukkan suhu dalam celcius "))
